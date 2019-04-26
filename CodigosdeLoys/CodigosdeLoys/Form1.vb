04aa52879a5e08878c258e104fa94c3ccf53b373
Miguel123x <40920612+Miguel123x@users.noreply.github.com>
2019-04-25 23:42:20 -0400

    Add files via upload

---
+++ CodigosdeLoys/CodigosdeLoys/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            ''comprobacion los dato del texbox en la tabla en BD
+            If usuarioRegistrado(txtUsuario.Text) = True Then ''evalua user
+
+                Dim contra As String = contrasena(txtUsuario.Text)
+                If contra.Equals(txtContrasena.Text) = True Then ''evalua contrasena
+                    FrmPrincipal.ShowDialog() ''llamar el formulario principal
+
+                Else
+                    MsgBox("Contraseña Invalida", MsgBoxStyle.Critical)
+                End If
+            Else
+                MsgBox("El Usuario: " + txtUsuario.Text + " no se encuentra registrado")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+End Class
---
+++ CodigosdeLoys/CodigosdeLoys/Module1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+Module Conexion
+    Public conexion As SqlConnection
+    Public enunciado As SqlCommand
+    Public respuesta As SqlDataReader
+
+
+    ''procedimiento abrir
+    Sub abrir()
+        Try
+            ''cadena de conexion 
+            conexion = New SqlConnection("Data Source=.;Initial Catalog=Login;Integrated Security=True")
+            ''abriendo la conexion y mensaje de conectado
+            conexion.Open()
+            MsgBox("Conectado")
+        Catch ex As Exception
+            MsgBox("No se pudo conectar" + ex.ToString) '' el + ex.ToString es para que me diga por que no se pudo conectar 
+        End Try
+    End Sub
+
+    ''funcion que me evalua si el usuario esta o no registrado 
+    Function usuarioRegistrado(ByVal nombreUsuario As String) As Boolean
+        Dim resultado As Boolean = False
+        Try
+            ''ejecutando el comando select en la tabla usuarios consulta
+            enunciado = New SqlCommand("Select * from Usuarios where Usuarios='" & nombreUsuario & "'", conexion)
+            respuesta = enunciado.ExecuteReader
+
+            '' si en la consulta encuentra un registo con lo ingresado en los texbox se ejecuta el sigunete if
+            If respuesta.Read Then
+                resultado = True
+            End If
+            respuesta.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resultado
+    End Function
+
+    ''evaluar la contrasena del usaurio en la tabla usaurio 
+    Function contrasena(ByVal nombreUsuario As String) As String
+        Dim resultado As String = ""
+        Try
+            enunciado = New SqlCommand("Select Contrasena from Usuarios where Usuarios='" & nombreUsuario & "'", conexion)
+            respuesta = enunciado.ExecuteReader
+
+            If respuesta.Read Then
+                resultado = respuesta.Item("Contrasena")
+            End If
+            respuesta.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resultado
+    End Function
+
+End Module
